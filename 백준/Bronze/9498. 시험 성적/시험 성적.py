a73205c300aa3dfe3ca72ad48c2b86f3205c5460
yunhwanhee <yunhwanhee00293@branksome.asia>
2024-12-17 18:18:02 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+
+if num1 >= 90 and num1 <= 100:
+    print("A")
+elif num1 >=80 and num1 <90:
+    print("B")
+elif num1 >=70 and num1 < 80:
+    print("C")
+elif num1 >=60 and num1 < 70:
+    print("D")
+else:
+    print("F")
